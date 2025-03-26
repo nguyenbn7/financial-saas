745e9a821d96de67398f4dad848031fdea0cbbbd
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-03-26 17:28:36 +0700

    update creatable select

--- src/lib/components/select/creatable-select.svelte
+++ src/lib/components/select/creatable-select.svelte
@@ -1,28 +1,144 @@
 <script lang="ts">
+	import { tick } from 'svelte';
+
+	import { computeCommandScore, Command as CommandPrimitive } from 'bits-ui';
+
+	import { cn } from '$lib/utils';
+
 	import { Input } from '../ui/input';
 	import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+	import { Command, CommandGroup, CommandItem, CommandList } from '../ui/command';
+
+	import Check from '@lucide/svelte/icons/check';
+	import ChevronsUpDown from '@lucide/svelte/icons/chevrons-up-down';
+	import CirclePlus from '@lucide/svelte/icons/circle-plus';
+
+	type Option = { label: string; value: string };
 
 	interface Props {
 		onCreate?: (value: string) => MaybePromise<void>;
-		options?: { label: string; value: string }[];
+		onChange?: (option: Option) => MaybePromise<void>;
+		options: Option[];
 		disabled?: boolean;
-		defaultValue?: string;
 	}
 
-	let { onCreate, options, disabled = false, defaultValue = $bindable('') }: Props = $props();
+	let { onCreate, onChange, options, disabled = false }: Props = $props();
+
 	let open = $state(false);
 	let value = $state('');
+	let query = $state('');
+
+	let triggerRef = $state<HTMLButtonElement>(null!);
+	let likelihood = $state(0);
+
+	let selectedLabel = $derived(options.find((f) => f.value === value)?.label);
+
+	function customFilter(commandValue: string, search: string, commandKeywords?: string[]): number {
+		const score = computeCommandScore(commandValue, search, commandKeywords);
+
+		likelihood = score;
+
+		return score;
+	}
+
+	// We want to refocus the trigger button when the user selects
+	// an item from the list so users can continue navigating the
+	// rest of the form with the keyboard.
+	function closeAndFocusTrigger() {
+		open = false;
+		tick().then(() => {
+			triggerRef.focus();
+		});
+	}
+
+	async function handleOnCreate() {
+		if (onCreate && query) {
+			await onCreate(query);
+		}
+
+		open = false;
+		query = '';
+	}
+
+	$effect(() => {
+		if (!open) query = '';
+	});
+
+	$inspect(selectedLabel);
 </script>
 
+{#snippet CommandAddItem()}
+	<button
+		tabindex={0}
+		onclick={handleOnCreate}
+		onkeydown={async (ev) => {
+			if (ev.key === 'Enter') {
+				await handleOnCreate();
+			}
+		}}
+		class={cn(
+			'flex w-full text-blue-500 cursor-pointer text-sm px-2 py-1.5 rounded-sm items-center focus:outline-none',
+			'hover:bg-blue-200 focus:!bg-blue-200'
+		)}
+	>
+		<CirclePlus size={16} class="mr-2 shrink-0" />
+		Create
+		<span class="overflow-ellipsis overflow-x-hidden whitespace-nowrap">"{query}"</span>
+	</button>
+{/snippet}
+
 <Popover bind:open>
-	<PopoverTrigger>
+	<PopoverTrigger bind:ref={triggerRef}>
 		{#snippet child({ props })}
-			<Input {...props} type={!open ? 'button' : 'text'} bind:value />
+			<!-- if not  -->
+			<Input
+				{...props}
+				type={!open ? 'button' : 'text'}
+				value={!open ? selectedLabel : query}
+				class="w-[200px]"
+				role="listbox"
+				aria-expanded={open}
+				tabindex={0}
+				{disabled}
+				oninput={(e) => {
+					query = e.currentTarget.value;
+				}}
+			/>
 		{/snippet}
 	</PopoverTrigger>
-	<PopoverContent>
-		<!-- <Command>
-			
-		</Command> -->
+
+	<PopoverContent class="w-[200px] p-0" sideOffset={10} trapFocus={false}>
+		<Command filter={customFilter}>
+			<CommandPrimitive.Input hidden value={query} />
+
+			{#if query && likelihood < 1}
+				<div class="p-1 text-sm">
+					{@render CommandAddItem()}
+				</div>
+			{/if}
+
+			{#if options.length === 0 && !query}
+				<div class="py-5 text-center text-sm">No items</div>
+			{/if}
+
+			<CommandList>
+				<CommandGroup class="overflow-y-auto">
+					{#each options as option}
+						<CommandItem
+							tabindex={0}
+							value={option.label}
+							onSelect={() => {
+								onChange?.(option);
+								value = option.value;
+								closeAndFocusTrigger();
+							}}
+						>
+							<Check class={cn('mr-2 size-4', value !== option.value && 'text-transparent')} />
+							{option.label}
+						</CommandItem>
+					{/each}
+				</CommandGroup>
+			</CommandList>
+		</Command>
 	</PopoverContent>
 </Popover>
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,7 +1,39 @@
 <script>
 	import { CreatableSelect } from '$lib/components/select';
+
+	const frameworks = $state([
+		{
+			value: 'sveltekit',
+			label: 'SvelteKit'
+		},
+		{
+			value: 'next.js',
+			label: 'Next.js'
+		},
+		{
+			value: 'nuxt.js',
+			label: 'Nuxt.js'
+		},
+		{
+			value: 'remix',
+			label: 'Remix'
+		},
+		{
+			value: 'astro',
+			label: 'Astro'
+		}
+	]);
 </script>
 
-<div class="w-32 mx-auto">
-	<CreatableSelect />
+<div class="w-32 mx-auto mt-10">
+	<CreatableSelect
+		options={frameworks}
+		onCreate={(value) => {
+			frameworks.push({
+				value,
+				label: value
+			});
+			console.log(value);
+		}}
+	/>
 </div>

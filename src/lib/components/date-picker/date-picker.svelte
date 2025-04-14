4cb97f57b8c07bf0978c524ef102dbdc6acb084a
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-04-14 22:42:42 +0700

    date picker

---
+++ src/lib/components/date-picker/date-picker.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { DateFormatter, type DateValue, getLocalTimeZone } from '@internationalized/date';
+	import { cn } from '$lib/utils';
+	import { Button } from '$lib/components/ui/button';
+	import { Calendar } from '$lib/components/ui/calendar';
+	import { Popover, PopoverTrigger, PopoverContent } from '$lib/components/ui/popover';
+	import CalendarIcon from '@lucide/svelte/icons/calendar';
+
+	const df = new DateFormatter('en-US', {
+		dateStyle: 'long'
+	});
+
+	let contentRef = $state<HTMLElement | null>(null);
+
+	interface Props {
+		value?: DateValue;
+		disabled?: boolean;
+	}
+
+	let { value = $bindable(), disabled = false }: Props = $props();
+</script>
+
+<Popover>
+	<PopoverTrigger>
+		{#snippet child({ props })}
+			<Button
+				{...props}
+				{disabled}
+				variant="outline"
+				class={cn('w-full justify-start text-left font-normal', !value && 'text-muted-foreground')}
+			>
+				<CalendarIcon class="size-4 mr-2" />
+				{value ? df.format(value.toDate(getLocalTimeZone())) : 'Pick a date'}
+			</Button>
+		{/snippet}
+	</PopoverTrigger>
+	<PopoverContent bind:ref={contentRef} class="w-auto p-0">
+		<Calendar type="single" bind:value {disabled} initialFocus />
+	</PopoverContent>
+</Popover>

b2e2936e182b8eeadab5347ff0c6cee6b5001c0e
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-04-15 13:26:15 +0700

    input currency

--- src/lib/components/ui/input/input.svelte
+++ src/lib/components/ui/input/input.svelte
@@ -7,7 +7,16 @@
 
 	type Props = WithElementRef<
 		Omit<HTMLInputAttributes, 'type'> &
-			({ type: 'file'; files?: FileList } | { type?: InputType; files?: undefined })
+			({ type: 'file'; files?: FileList } | { type?: InputType; files?: undefined }) &
+			(
+				| {
+						type: 'currency';
+						fractionDigits?: number;
+						locales?: Intl.LocalesArgument;
+						options?: Intl.NumberFormatOptions;
+				  }
+				| { type?: InputType; locales?: undefined; options?: undefined }
+			)
 	>;
 
 	let {
@@ -16,8 +25,34 @@
 		type,
 		files = $bindable(),
 		class: className,
+		locales = 'en-US',
+		options = {
+			style: 'currency',
+			currency: 'USD',
+			maximumFractionDigits: 2,
+			minimumFractionDigits: 2
+		},
 		...restProps
 	}: Props = $props();
+
+	const currencyFormatter = Intl.NumberFormat(locales, options);
+
+	const fractionDigits = Math.pow(10, options.maximumFractionDigits ?? 2);
+
+	let currency = $state(currencyFormatter.format(Number(value) / fractionDigits));
+
+	if (type === 'currency') {
+		let trackingNumValue = $derived(Number(value));
+
+		$effect(() => {
+			let numericValue = Number(currency.replace(/\D/g, ''));
+
+			if (!isNaN(trackingNumValue) && numericValue !== trackingNumValue)
+				numericValue = trackingNumValue;
+
+			currency = currencyFormatter.format(numericValue / fractionDigits);
+		});
+	}
 </script>
 
 {#if type === 'file'}
@@ -30,6 +65,24 @@
 		type="file"
 		bind:files
 		bind:value
+		{...restProps}
+	/>
+{:else if type === 'currency'}
+	<input
+		bind:this={ref}
+		class={cn(
+			'border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+			className
+		)}
+		type="text"
+		bind:value={
+			() => currency,
+			(newValue) => {
+				currency = newValue;
+				const numericValue = Number(currency.replace(/\D/g, ''));
+				value = numericValue;
+			}
+		}
 		{...restProps}
 	/>
 {:else}
